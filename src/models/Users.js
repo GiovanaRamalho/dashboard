fc6ad28478a5ac4bc4652597b6e4cd755200ecf0
GiovanaRamalho <giovanasr854@gmail.com>
2024-07-23 19:08:48 -0300

    creating user model

---
+++ src/models/Users.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const User = mongoose.model(
+  "User",
+  new Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+    },
+    { timestamps: true }
+  )
+);
